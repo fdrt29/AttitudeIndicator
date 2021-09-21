d207f230d2fcc550156a0664401470fe8a170496
Fedor Teleshov <fdrt29@gmail.com>
2021-09-21 19:31:24 +0300

    init commit
    Signed-off-by: Fedor Teleshov <fdrt29@gmail.com>

---
+++ GyroHorizon.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace GyroHorizon
+{
+    public partial class GyroHorizon : UserControl
+    {
+        public GyroHorizon()
+        {
+            InitializeComponent();
+        }
+    }
+}
