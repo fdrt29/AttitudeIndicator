96e5f1b8c93d4ecb5520ab25a7d5264d69df1c32
Fedor Teleshov <fdrt29@gmail.com>
2021-09-24 14:33:15 +0300

    indicator starts flashing when excess valid roll or pitch
    Signed-off-by: Fedor Teleshov <fdrt29@gmail.com>

--- GyroHorizonUI.xaml.cs
+++ GyroHorizonUI.xaml.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using GyroHorizon.Annotations;
 
 namespace GyroHorizon
@@ -21,8 +20,9 @@ namespace GyroHorizon
 
 
         private GyroHorizonVM _vm;
-
-        private double _yOffset = 0;
+        private double _yOffset;
+        private bool _rollExcess;
+        private bool _pitchExcess;
 
         public double YOffset
         {
@@ -34,13 +34,49 @@ namespace GyroHorizon
             }
         }
 
+        public bool RollExcess
+        {
+            get => _rollExcess;
+            set
+            {
+                _rollExcess = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool PitchExcess
+        {
+            get => _pitchExcess;
+            set
+            {
+                _pitchExcess = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         #region Dependency Properties // TODO add coerce or validate and check other
 
         #region DependencyProperty Roll
 
         public static readonly DependencyProperty RollProperty = RollProperty = DependencyProperty.Register("Roll",
-            typeof(double), typeof(GyroHorizonUI));
+            typeof(double), typeof(GyroHorizonUI), new FrameworkPropertyMetadata(RollChangedCallback));
+
+        private static void RollChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //TODO value if > ValidRoll call some
+            if (!(d is GyroHorizonUI gyroHorizonUi)) return;
+            if (!double.TryParse(e.NewValue.ToString(), out double value)) return;
+
+            if (Math.Abs(value) > gyroHorizonUi.ValidRoll && gyroHorizonUi.RollExcess == false)
+            {
+                gyroHorizonUi.RollExcess = true;
+            }
+            else if (gyroHorizonUi.RollExcess == true && Math.Abs(value) <= gyroHorizonUi.ValidRoll)
+            {
+                gyroHorizonUi.RollExcess = false;
+            }
+        }
 
         public double Roll
         {
@@ -54,11 +90,21 @@ namespace GyroHorizon
 
         public static readonly DependencyProperty PitchProperty = DependencyProperty.Register("Pitch", typeof(double),
             typeof(GyroHorizonUI),
-            new FrameworkPropertyMetadata(PropertyChangedCallback));
+            new FrameworkPropertyMetadata(PitchChangedCallback));
 
-        private static void PropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void PitchChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             if (!(d is GyroHorizonUI gyroHorizonUi)) return;
+            if (!double.TryParse(e.NewValue.ToString(), out double value)) return;
+
+            if (Math.Abs(value) > gyroHorizonUi.ValidPitch && gyroHorizonUi.PitchExcess == false)
+            {
+                gyroHorizonUi.PitchExcess = true;
+            }
+            else if (gyroHorizonUi.PitchExcess == true && Math.Abs(value) <= gyroHorizonUi.ValidPitch)
+            {
+                gyroHorizonUi.PitchExcess = false;
+            }
 
             gyroHorizonUi.YOffset = gyroHorizonUi.ConvertPitchToYOffset(gyroHorizonUi.Pitch);
         }
